970b282e3dd57fbc23ac4217de69a288eabacc2c
yurandeng <yurandeng@qq.com>
2021-09-12 18:46:56 +0800

    hot-fix commit

--- Yrd-TestDemo/src/main/java/com/yrd/domain/git_tst.java
+++ Yrd-TestDemo/src/main/java/com/yrd/domain/git_tst.java
@@ -1,13 +1,15 @@
 package com.yrd.domain;
 
 /**
- * @ClassName: git_tst
- * @Description:
- * @author: Yrd
- * @date: 2021-09-12 18:01
+ * ClassName: git_tst
+ * Description:
+ * author: Yrd
+ * date: 2021-09-12 18:01
  */
 public class git_tst {
     public static void main(String[] args) {
+        System.out.println("helwwlo");
+        System.out.println("helwwlo");
         System.out.println("helwwlo");
         System.out.println("helwwlo");
     }
