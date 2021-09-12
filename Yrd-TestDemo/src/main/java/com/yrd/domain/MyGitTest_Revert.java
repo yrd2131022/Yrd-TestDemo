7cd86962dd5aa2a5aab6ecc2a1b617601db3de6d
yurandeng <yurandeng@qq.com>
2021-09-12 19:36:03 +0800

    MyGitTest_Rv ert commit 1

---
+++ Yrd-TestDemo/src/main/java/com/yrd/domain/MyGitTest_Revert.java
@@ -0,0 +1,13 @@
+package com.yrd.domain;
+
+/**
+ * ClassName: MyGitTest_Revert
+ * Description:
+ * author: Yrd
+ * date: 2021-09-12 19:33
+ */
+public class MyGitTest_Revert {
+    public static void main(String[] args) {
+        System.out.println("Revert");
+    }
+}
